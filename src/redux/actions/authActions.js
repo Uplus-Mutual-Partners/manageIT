e57c831310f424fc9825cf9a58977005b6d8d567
uwadonat <uwadonat@gmail.com>
2021-12-08 12:46:04 +0300

    Add action

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,35 +1,31 @@
-import {
-  VIEW_PROFILE_ERROR,
-  VIEW_PROFILE_SUCCESS
-} from "../actionTypes/profileActionTypes";
+import { VIEW_USERS_INFO } from "../actionTypes/profileActionTypes";
 
 import apiCall from "../../helpers/apiCall";
 
-export const viewProfileAction = () => async (dispatch) => {
+export const viewUsersProfileAction = () => async (dispatch) => {
   try {
     const body = {
-      action:"viewProfile"
+      action: "VIEW_USERS_INFO",
     };
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJ1c2VySW1hZ2UiOiJodHRwczpcL1wvZmlyZWJhc2VzdG9yYWdlLmdvb2dsZWFwaXMuY29tXC92MFwvYlwvbGVhcm5iYXNlLWJhYTZkLmFwcHNwb3QuY29tXC9vXC80MDMyZDY0OS02YWZlLTQ1OTItYjkyZS0yZGU4Y2I4YmNmM2U_YWx0PW1lZGlhJnRva2VuPWU2MDU2NWM4LTRhZWItNDhlYi1iOTUwLTAyMTg5YzAyYWY4OCIsInBob25lIjoiMjUwNzgxOTcyOTQyIiwidXNlck5hbWUiOiJFbW1hbnVlbCIsInJvbGUiOiJjbGllbnQiLCJpYXQiOjE2MTEzNDE4Nzd9._VcWpswpkNJjtR4Lml-tLQttVbzAsNf-hLK9goL6BXs";
-    const viewProfileResponse = await apiCall.post("/index",
-    body,
-    {
+
+    const viewUsersInfoResponse = await apiCall.post("/users", body, {
       headers: {
         "Content-Type": "application/json",
-        "x-auth-token": `${token}`
-      }
+      },
     });
     dispatch({
-      type: VIEW_PROFILE_SUCCESS,
-      payload: viewProfileResponse.data
+      type: VIEW_USERS_INFO,
+      payload: viewUsersInfoResponse.data,
     });
-   console.log('_____________RESPONSE_________',viewProfileResponse.data.data);
+    console.log(
+      "_____________RESPONSE_________",
+      viewUsersInfoResponse.data.data
+    );
   } catch (error) {
     //console.log('__________________ERROR_____', error);
     dispatch({
       type: VIEW_PROFILE_ERROR,
-      payload: error.response
+      payload: error.response,
     });
   }
 };
